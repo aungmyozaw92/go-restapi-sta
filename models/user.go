227a8d94b5d6b6835b0ed9bd3c2608994cdff368
marathonamz <aungmyozaw@marathonmyanmar.com>
2024-11-07 14:21:20 +0630

    added auth

--- models/user.go
+++ models/user.go
@@ -1,6 +1,14 @@
 package models
 
-import "time"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/aungmyozaw92/go-restapi-sta/config"
+	"github.com/aungmyozaw92/go-restapi-sta/utils"
+	"golang.org/x/crypto/bcrypt"
+)
 
 
 type User struct {
@@ -46,4 +54,106 @@ type LoginInfo struct {
 type AllowedModule struct {
 	ModuleName     string `json:"module_name"`
 	AllowedActions string `json:"allowed_actions"`
+}
+
+func (result *User) PrepareGive() {
+	result.Password = ""
+}
+
+// node
+// returns decoded curosr string
+func (s User) GetCursor() string {
+	return s.CreatedAt.String()
+}
+
+
+func Login(ctx context.Context, username string, password string) (*LoginInfo, error) {
+
+	db := config.GetDB()
+	var err error
+	var result LoginInfo
+
+	user := User{}
+
+	err = db.WithContext(ctx).Model(User{}).Where("username = ?", username).Take(&user).Error
+	if err != nil {
+		return &result, errors.New("invalid username or password")
+	}
+	err = utils.ComparePassword(user.Password, password)
+
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return &result, errors.New("invalid username or password")
+	}
+
+	isActive := *user.IsActive
+	if !isActive {
+		return &result, errors.New("user is disabled")
+	}
+	token, err := utils.JwtGenerate(user.ID)
+	result.Token = token
+	result.UserId = user.ID
+	result.Name = user.Name
+	result.Username = user.Username
+	result.Email = user.Email
+	result.Phone = user.Phone
+	result.ImageUrl = user.ImageUrl
+
+	// if user.RoleId == 0 {
+	// 	return nil, errors.New("please assign role")
+	// } else {
+	// 	var userRole Role
+	// 	if err := db.WithContext(ctx).Model(&Role{}).
+	// 		Preload("RoleModules").Preload("RoleModules.Module").
+	// 		Where("id = ?", user.RoleId).First(&userRole, user.RoleId).Error; err != nil {
+	// 		return nil, err
+	// 	}
+	// 	result.Role = userRole.Name
+	// 	var allowedModules []AllowedModule
+	// 	for _, rm := range userRole.RoleModules {
+	// 		allowedModules = append(allowedModules, AllowedModule{
+	// 			ModuleName:     rm.Module.Name,
+	// 			AllowedActions: rm.AllowedActions,
+	// 		})
+	// 	}
+	// 	result.Modules = allowedModules
+	// }
+
+	if err != nil {
+		return &result, err
+	}
+
+	return &result, nil
+}
+
+// destroy current session
+// func Logout(ctx context.Context) (bool, error) {
+// 	token, ok := utils.GetTokenFromContext(ctx)
+// 	if !ok || token == "" {
+// 		return false, errors.New("token is required")
+// 	}
+
+// 	// Invalidate the token by storing it in Redis with an expiration
+// 	expiration := time.Hour // Match this with your JWT token's expiration time
+// 	err := config.SetRedisValue(token, "invalid", expiration)
+// 	if err != nil {
+// 		return false, err
+// 	}
+
+// 	return true, nil
+// }
+
+func GetUser(ctx context.Context, id int) (*User, error) {
+
+	db := config.GetDB()
+	var result User
+
+	err := db.WithContext(ctx).First(&result, id).Error
+
+	if err != nil {
+		return &result, errors.New("record not found")
+	}
+
+	result.PrepareGive()
+
+	return &result, nil
 }
--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,8 @@ package routes
 import (
 	"net/http"
 
+	"github.com/aungmyozaw92/go-restapi-sta/controllers"
+	"github.com/aungmyozaw92/go-restapi-sta/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +14,12 @@ func SetupRoutes(r *gin.Engine) {
 		ctx.JSON(http.StatusOK, "welcome home")
 	})
 
+	authRouter := r.Group("/api/v1")
+	authRouter.POST("/login", controllers.Login)
 	
-	
+	protectedRouter := r.Group("/api/v1")
+	protectedRouter.Use(middlewares.AuthMiddleware())
+
+	protectedRouter.GET("/profile", controllers.Profile)
+	// protectedRouter.POST("/logout", controllers.Logout)
 }
